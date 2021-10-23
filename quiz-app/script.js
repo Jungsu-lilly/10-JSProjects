a14c79e2bfe1f636e4541405b9bcff39e7bf1fde
matt1235 <matt1235@naver.com>
2021-10-23 14:45:55 +0900

    deSelected, submitBtn listener 추가

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -10,7 +10,7 @@ const quizData = [
         question: 'What is the most used programming language in 2021?',
         a: 'java',
         b: 'C++',
-        C: 'Python',
+        c: 'Python',
         d: 'JavaScript',
         correct: 'a'
     },{
@@ -44,6 +44,7 @@ const quizData = [
     }
 ];
 
+const answerEls = document.querySelectorAll('.answer');
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
@@ -52,16 +53,64 @@ const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
+let answer= undefined;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz(){
+    deselectAnswers();
+
     const currentQuizData =  quizData[currentQuiz];
     questionEl.innerText = currentQuizData.question;
 
-    a_text.innerText =  currentQuizData.a;
+    a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
 
 }
+
+function getSelected(){
+    
+    answer = undefined;
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked){
+            answer = answerEl.id;
+        } //else{
+        //     console.log('노체크!');
+        // }
+    });
+
+    return answer;
+}
+
+function deselectAnswers(){
+    answerEls.forEach(a => {
+        a.checked = false;
+    })
+}
+
+submitBtn.addEventListener('click', ()=>{
+    
+    const answer = getSelected();
+    // check to see the answer
+    // console.log(answer);
+    if(answer){
+        if(answer === quizData[currentQuiz].correct){
+            score++;
+        }
+
+        currentQuiz++;
+        if(currentQuiz < quizData.length){
+            loadQuiz();
+        }
+        else{
+            // TODO: Show results
+            alert('You finished! Thanks for submission')
+        }
+    }
+
+})
+
