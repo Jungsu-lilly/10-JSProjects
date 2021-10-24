ec3cccc4e15fe439e365d81bf49f980f614170f8
matt1235 <matt1235@naver.com>
2021-10-24 21:51:37 +0900

    meals, meal 클래스 (Random Recipe)

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -53,6 +53,7 @@ const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
 
+
 let currentQuiz = 0;
 let answer= undefined;
 let score = 0;
