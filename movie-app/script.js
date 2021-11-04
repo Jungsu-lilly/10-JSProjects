ccd2305139c5e7dcc5bd58dd40b906b638fc2bda
matt1235 <matt1235@naver.com>
2021-11-04 15:33:04 +0900

    moviedb api key 변경(새로 발급 받음)

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,8 +1,8 @@
-const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c34435b5564f5a284f362fe8f64dda7a&page=1";
 
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
-    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+    "https://api.themoviedb.org/3/search/movie?&api_key=c34435b5564f5a284f362fe8f64dda7a&query=";
 
 const main = document.querySelector('main');
 const form = document.getElementById('form');
@@ -17,11 +17,9 @@ async function getMovies(url){
     const resp = await fetch(url);
     const respData = await resp.json();
 
-    console.log(respData);
-
     showMovies(respData.results);
-
 }
+
 
 function showMovies(movies){
 
@@ -37,14 +35,14 @@ function showMovies(movies){
         movieEl.innerHTML = `
             <img 
                 src="${IMGPATH + poster_path}" 
-                alt="${title}">
+                alt="no image: ${title}">
             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class = "${getClassByRate
                 (vote_average)}">${vote_average}</span>
             </div>
             <div class="overview">
-                    ${overview}
+                ${overview}
             </div>
         `;
 
